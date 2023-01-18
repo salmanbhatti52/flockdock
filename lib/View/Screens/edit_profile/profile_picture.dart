5985e49c229c97df361bad1539a09eff495e18de
hammad <hammadraza5388@gmail.com>
2023-01-18 15:54:00 +0500

    Update profile_picture.dart

--- lib/View/Screens/edit_profile/profile_picture.dart
+++ lib/View/Screens/edit_profile/profile_picture.dart
@@ -17,6 +17,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EditProfilePicture extends StatefulWidget {
