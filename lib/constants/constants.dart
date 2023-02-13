7beb8e2904cea944823f28f227284abaffa87480
Muhammad Adnan Ameen <90311877+Dani4772@users.noreply.github.com>
2023-02-13 13:08:16 +0500

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -39,7 +39,6 @@ class AppConstants {
   static const String apiKey = "AIzaSyCabuXEjXibCoxdTGTYs7knEbxBRYjpVpE";
   //static const String apiUrl = 'https://dev.eigix.com/flockDock/api/';
   static const String apiUrl = 'https://flockdock.eigix.net/api/';
-  static const String imageUrl1 = 'https://flockdock.eigix.net/public/uploads/group_cover_photos/';
   static const String register = 'signup';
   static const String placeholder = 'https://th.bing.com/th/id/R.3e77a1db6bb25f0feb27c95e05a7bc57?rik=DswMYVRRQEHbjQ&riu=http%3a%2f%2fwww.coalitionrc.com%2fwp-content%2fuploads%2f2017%2f01%2fplaceholder.jpg&ehk=AbGRPPcgHhziWn1sygs8UIL6XIb1HLfHjgPyljdQrDY%3d&risl=&pid=ImgRaw&r=0';
 
